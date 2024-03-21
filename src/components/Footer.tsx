f2da6ac934b404a8ab5a16673478c9e339ab4969
John Larson <jl@buildmidwestern.com>
2024-03-21 07:38:18 -0500

    feat: updated logo, contact page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -117,7 +117,7 @@ export function Footer() {
         </div>
         <div className='mb-20 mt-24 flex flex-wrap items-end justify-between gap-x-6 gap-y-4 border-t border-neutral-950/10 pt-12'>
           <Link href='/' aria-label='Home'>
-            <div className='h-24'>
+            <div className='h-24 text-neutral-950'>
               <Logo />
             </div>
           </Link>
