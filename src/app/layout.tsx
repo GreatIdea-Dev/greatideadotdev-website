d4906965431a8a04907ff964dbf92604ba2d94b8
John Larson <jl@buildmidwestern.com>
2024-03-28 08:56:39 -0500

    feat: installed anaylitics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { type Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/react';
 
 import { RootLayout } from '@/components/RootLayout';
 
@@ -15,6 +16,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en' className='h-full bg-neutral-950 text-base antialiased'>
       <body className='flex min-h-full flex-col'>
+        <Analytics />
         <RootLayout>{children}</RootLayout>
       </body>
     </html>
